7c8d5a9f250dfe6bad13fa6d9a6bd3f483ad70b8
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2024-04-30 13:00:06 -0400

    attempt to add others, admin works

--- access_control.py
+++ access_control.py
@@ -5,26 +5,34 @@ username = "neo4j"
 password = "FHoYxcA5XDWYfeKQq2F7HJgtIvsukiyxChVtj2adJ0I"
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
-def check_access(user_name, action):
+def check_access(user_name, action, resource_name):
     with driver.session() as session:
         result = session.run(
             """
-            MATCH (user:User {name: $user_name})-[:HAS_ROLE]->(role)-[:HAS_POLICY]->(policy)-[:HAS_PERMISSION]->(permission)
-            RETURN COUNT(*) > 0 AS allowed
+            MATCH (user:User {name: $user_name})-[:HAS_ROLE]->(role:Role)-[:HAS_POLICY]->(policy)-[:CAN_READ|CAN_WRITE|CAN_CREATE|CAN_DELETE]->(resource:Resource {name: $resource_name})
+            RETURN user.name AS user, role.name AS role, policy.name AS policy, resource.name AS resource, $action AS action, COUNT(*) > 0 AS allowed
             """,
-            user_name=user_name
-        )
+            user_name=user_name,
+            resource_name=resource_name,
+            action=action.upper()  
+            )
         record = result.single()
-        allowed = record["allowed"]
-        if allowed:
-            print(f"{user_name} is allowed to {action}.")
+
+        if record:
+            allowed = record["allowed"]
+            if allowed:
+                print(f"{record['user']} is allowed to {action} {resource_name}")
+            else:
+                print(f"{record['user']} is not allowed to {action} {resource_name}")
         else:
-            print(f"{user_name} is not allowed to {action}.")
+            print(f"No records for {user_name}.")
+        print(f"User: {record['user']}, Role: {record['role']}, Policy: {record['policy']}, Resource: {record['resource']}, Action: {record['action']}, Allowed: {allowed}")
 
 user_name = input("Enter your name: ").lower()
-action = input("Enter the action you want to perform: ").lower()
+action = input("Enter the action you want to perform (e.g., read, write, delete): ").lower()
+resource_name = input("Enter the resource name: ")
 
-check_access(user_name, action)
+check_access(user_name, action, resource_name)
 
 
 
